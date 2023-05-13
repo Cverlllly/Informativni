a0d560def897974a23d5c0af87c1652fc121741f
Cverlllly <anej.cverle@scv.si>
2023-05-13 18:22:14 +0200

    wip

---
+++ details.php
@@ -0,0 +1,92 @@
+<head>
+    <link rel="stylesheet" href="form.css">
+</head>
+<?php
+
+include 'navbar.php';
+$id = $_GET['id'];
+$sql = "SELECT * FROM prijava p inner join sole s on p.sola_id=s.sola_id inner join kontaktni_podatki k on p.kontakt_id= k.kontakt_id WHERE p.prijava_id=$id";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+?>
+
+<div class="container">
+  <h2>Podatki</h2>
+  <form>
+    <div class="form-group">
+      <label for="ime_sole">Ime šole:</label>
+      <input type="text" id="ime_sole" name="ime_sole" value="<?php echo $row['ime_sole']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="ime">Kontaktna oseba: </label>
+      <button type="button" onclick="showDetails()" class="btn btn-primary">
+        <?php echo $row['ime'] . ' ' . $row['priimek']; ?>
+      </button>
+    </div>
+    <div id="details" style="display: none;">
+      <h3>Dodatni podatki:</h3>
+      <p>Email: <?php echo $row['email']; ?></p>
+      <p>Telefon: <?php echo $row['telefonska']; ?></p>
+    </div><br>
+    <div class="form-group">
+      <label for="nacin_izvedbe">Način izvedbe:</label>
+      <input type="text" id="nacin_izvedbe" name="nacin_izvedbe" value="<?php echo $row['nacin_izvedbe']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="datum_obiska">Datum obiska:</label>
+      <input type="text" id="datum_obiska" name="datum_obiska" value="<?php echo $row['datum_obiska']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="urnik_obiska">Urnik obiska:</label>
+      <input type="text" id="urnik_obiska" name="urnik_obiska" value="<?php echo $row['urnik_obiska']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="starostna_skupina_otrok">Starostna skupina otrok:</label>
+      <input type="text" id="starostna_skupina_otrok" name="starostna_skupina_otrok" value="<?php echo $row['starostna_skupina_otrok']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="st_ucencev">Število učencev:</label>
+      <input type="text" id="st_ucencev" name="st_ucencev" value="<?php echo $row['st_ucencev']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="pricakovanja">Pričakovanja:</label>
+      <input type="text" id="pricakovanja" name="pricakovanja" value="<?php echo $row['pricakovanja']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="sredstva">Sredstva:</label>
+      <input type="text" id="sredstva" name="sredstva" value="<?php echo $row['sredstva']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="sporocilo">Sporočilo</label><br>
+        <textarea id="sporocilo" name="sporocilo"><?php echo $row['sporocilo']; ?></textarea>
+    </div>
+    <div class="form-group">
+        <label for="cas_prijave">Čas prijave</label>
+        <input type="text" id="cas_prijave" name="cas_prijave" value="<?php echo $row['cas_prijave']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="sprejeta">Sprejeto</label>
+        <input type="text" id="sprejeta" name="sprejeta" value="<?php echo $row['sprejeta']; ?>" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="sola_id">Obisk šole</label>
+        <input type="text" id="sola_id" name="sola_id" value="<?php echo $row['ime']; ?>" class="form-control">
+    </div>
+  </form>
+
+</div>
+
+<script>
+function showDetails() {
+  var details = document.getElementById("details");
+  if (details.style.display === "none") {
+    details.style.display = "block";
+  } else {
+    details.style.display = "none";
+  }
+}
+</script>
+
+<?php
+$conn->close();
+?>
--- mainpage.php
+++ mainpage.php
@@ -1,35 +1,65 @@
 <?php
 include 'navbar.php';
+$_COOKIE['admin'];
+$admin = json_decode($_COOKIE['admin'], true);
+$id = $admin['ime'];
 
 // Check if the filter is submitted
-if(isset($_GET['filter'])) {
-  $filter = $_GET['filter'];
-  $sql = "SELECT * FROM prijava p inner join sole s on p.sola_id=s.sola_id inner join kontaktni_podatki k on p.kontakt_id= k.kontakt_id WHERE p.sprejeta = '$filter'";
+if ($id == 'vse') {
+  if (isset($_GET['filter'])) {
+    $filter = $_GET['filter'];
+    $sql = "SELECT * FROM prijava p 
+            INNER JOIN sole s ON p.sola_id = s.sola_id 
+            INNER JOIN kontaktni_podatki k ON p.kontakt_id = k.kontakt_id 
+            WHERE p.sprejeta = '$filter'";
+  } else {
+    $sql = "SELECT * FROM prijava p 
+            INNER JOIN sole s ON p.sola_id = s.sola_id 
+            INNER JOIN kontaktni_podatki k ON p.kontakt_id = k.kontakt_id";
+  }
 } else {
-  $sql = "SELECT * FROM prijava p inner join sole s on p.sola_id=s.sola_id inner join kontaktni_podatki k on p.kontakt_id= k.kontakt_id";
+  if (isset($_GET['filter'])) {
+    $filter = $_GET['filter'];
+    $sql = "SELECT * FROM prijava p 
+            INNER JOIN sole s ON p.sola_id = s.sola_id 
+            INNER JOIN kontaktni_podatki k ON p.kontakt_id = k.kontakt_id 
+            WHERE p.sprejeta = '$filter' AND s.ime='$id'";
+  } else {
+    $sql = "SELECT * FROM prijava p 
+            INNER JOIN sole s ON p.sola_id = s.sola_id 
+            INNER JOIN kontaktni_podatki k ON p.kontakt_id = k.kontakt_id 
+            WHERE s.ime='$id'";
+  }
 }
 
 $result = $conn->query($sql);
 
 ?>
+
 <div class="container-filter">
-<form action="" method="get">
-  <label for="filter">Filter by Done:</label>
-  <select id="filter" name="filter">
-    <option value="">All</option>
-    <option value="1">Complete</option>
-    <option value="0">In progress</option>
-  </select>
-  <button type="submit">Filter</button>
-</form>
+  <form action="" method="get">
+    <label for="filter">Filter by Done:</label>
+    <select id="filter" name="filter">
+      <option value="">All</option>
+      <option value="1">Complete</option>
+      <option value="0">In progress</option>
+    </select>
+    <button type="submit">Filter</button>
+  </form>
 </div>
+
+<br><br><br>
 
 <?php
 if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
+  while ($row = $result->fetch_assoc()) {
     echo "<div class='data-container'>";
     echo "<div class='data-row' data-id='" . $row["prijava_id"] . "'>";
-        echo  "<div>". $row["ime_sole"].'&nbsp'.$row["ime"].'&nbsp'.$row["priimek"].'&nbsp'.$row["datum_obiska"]." </div>";
+    echo $row["ime_sole"] . '&nbsp' . "<div id='test'>" . $row["ime"] . '&nbsp' . $row["priimek"] . "</div>";
+    echo "<div class='buttons-container'>";
+    echo "<button class='accept-button'>Accept</button>";
+    echo "<button class='delete-button'>Delete</button>";
+    echo "</div>";
     echo "</div>";
     echo "</div>";
   }
@@ -39,14 +69,16 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
+
 <script>
   var dataRows = document.querySelectorAll('.data-row');
   for (var i = 0; i < dataRows.length; i++) {
-    dataRows[i].addEventListener('click', function() {
+    dataRows[i].addEventListener('click', function () {
       var id = this.dataset.id;
       window.location.href = 'details.php?id=' + id;
     });
   }
 </script>
+
 </body>
 </html>
