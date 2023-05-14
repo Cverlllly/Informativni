46c4e39aefadc09ba2b3a251431c508c47cfd6df
Endersgamer12 <59827726+Endersgamer12@users.noreply.github.com>
2023-05-14 20:24:35 +0200

    mamo dela XD wip

---
+++ pozdrav.php
@@ -0,0 +1,15 @@
+<?php include "glava.php"; ?>
+
+<div class=container>
+
+    <div class="big_text" style="margin-top: 50px;">POZDRAVLJENI!</div>
+
+    <div class="medium_text" style="margin-top: 30px;">Prijava na informativni dan ŠC Velenje</div>
+    <form action="prijava_info.php">
+
+        <input class="butten" style="margin-top: 100px; width: 300px; height:80px" type="submit" value="PRIJAVA">
+    </form>
+</div>
+</body>
+
+</html>
