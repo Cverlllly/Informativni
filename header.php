c349ee19f95e3a5b776cb2ea22afb48a9d69143e
Endersgamer12 <59827726+Endersgamer12@users.noreply.github.com>
2023-05-14 13:32:41 +0200

    zdravo

--- header.php
+++ header.php
@@ -3,9 +3,8 @@ $host = 'localhost';
 $user = 'root';
 $pass = '';
 $dbname = 'informativni';
-
+#zdravo!
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
